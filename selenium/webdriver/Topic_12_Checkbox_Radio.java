bf363b4fb4b7d212eeb7b33b71edac3d1d211ed3
dinkuin <dinhthidiemquynh99@gmail.com>
2025-04-14 11:59:44 +0700

    14/4 Browser_Excercise_checkbox_radio

--- selenium/webdriver/Topic_12_Checkbox_Radio.java
+++ selenium/webdriver/Topic_12_Checkbox_Radio.java
@@ -1,6 +1,6 @@
 package webdriver;
 
-import graphql.Assert;
+import org.testng.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -13,16 +13,21 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import static java.lang.Thread.sleep;
+
 public class Topic_12_Checkbox_Radio {
 
     WebDriver driver;
+    JavascriptExecutor jsExecutor;
 
     @BeforeClass
     public void beforeClass() {
         driver = new FirefoxDriver();
+        jsExecutor = (JavascriptExecutor) driver;
+
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        }
+    }
 
     @Test
     public void TC_01_Telerik() {
@@ -48,7 +53,7 @@ public class Topic_12_Checkbox_Radio {
         }
         Assert.assertTrue(driver.findElement(dualZoneAirBy).isSelected());
 
-         // Deselect to "Dual-zone air conditioning" checkbox (bỏ chọn)
+        // Deselect to "Dual-zone air conditioning" checkbox (bỏ chọn)
         if (driver.findElement(dualZoneAirBy).isSelected()) {
             driver.findElement(dualZoneAirBy).click();
         }
@@ -75,7 +80,7 @@ public class Topic_12_Checkbox_Radio {
 
         // click all checkboxes
         for (WebElement checkbox : checkboxes) {
-            if (!checkbox.isSelected()){
+            if (!checkbox.isSelected()) {
                 checkbox.click();
             }
         }
@@ -93,7 +98,7 @@ public class Topic_12_Checkbox_Radio {
         }
 
         // Verify all checkboxes deselected
-        for (WebElement checkbox : checkboxes){
+        for (WebElement checkbox : checkboxes) {
             Assert.assertFalse(checkbox.isSelected());
         }
 
@@ -106,7 +111,7 @@ public class Topic_12_Checkbox_Radio {
 
         // cách khác
         for (WebElement checkbox : checkboxes) {
-            if (!checkbox.isSelected() && checkbox.getAttribute("value").equals("Heart Attack")){
+            if (!checkbox.isSelected() && checkbox.getAttribute("value").equals("Heart Attack")) {
                 checkbox.click();
             }
         }
@@ -114,10 +119,76 @@ public class Topic_12_Checkbox_Radio {
 
     }
 
+    @Test
+    public void TC_03_Ubunto() {
+        driver.get("http://login.ubuntu.com");
+
+        // Thẻ input: dùng để click
+        // Dùng để verify: isSelected()
+
+        // 1. Dùng thẻ input để click -> lỗi
+        // Click register button
+
+        // By newUserRadio = By.cssSelector("input#id_new_user");
+        // driver.findElement(newUserRadio).click();
+        // Assert.assertFalse(driver.findElement(newUserRadio).isSelected());
+
+        // 2. Dùng thẻ khác input để click -> pass
+        // Dùng thẻ đó để verify -> fail
+        // isSelected() -> Dùng cho thẻ input/option
+
+        // By newUserRadio = By.cssSelector("label.new-user");
+        // driver.findElement(newUserRadio).click();
+        // Assert.assertTrue(driver.findElement(newUserRadio).isSelected());
+
+        // 3.  Dùng thẻ khác input để click -> pass
+        // Dùng thẻ input để verify -> pass
+        // By newUserRadioLabel = By.cssSelector("input#id_new_user");
+        // By newUserRadioInput = By.cssSelector("label.new-user");
+        // driver.findElement(newUserRadioLabel).click();
+        // Assert.assertTrue(driver.findElement(newUserRadioInput).isSelected());
+
+        // 4. Dùng duy nhất thẻ input để click/ verify dùng JS Executor
+        By newUserRadioInput = By.cssSelector("input#id_new_user");
+        jsExecutor.executeScript("arguments[0].click()", driver.findElement(newUserRadioInput));
+        Assert.assertTrue(driver.findElement(newUserRadioInput).isSelected());
+
+        By termCheckbox = By.cssSelector("input#id_accept_tos");
+        jsExecutor.executeScript("arguments[0].click()", driver.findElement(termCheckbox));
+        Assert.assertTrue(driver.findElement(termCheckbox).isSelected());
+    }
+
+    @Test
+    public void TC_04_Docs() throws InterruptedException {
+        driver.get("https://docs.google.com/forms/d/e/1FAIpQLSfiypnd69zhuDkjKgqvpID9kwO29UCzeCVrGGtbNPZXQok0jA/viewform");
+        Thread.sleep(5000);
+
+        By hcmRadio = By.xpath("//div[@aria-label='Hồ Chí Minh']");
+        driver.findElement(hcmRadio).click();
+        //Assert.assertTrue(driver.findElement(By.xpath("//div[@aria-label='Hồ Chí Minh' and @aria-checked='true'")).isDisplayed());
+        Assert.assertEquals(driver.findElement(hcmRadio).getAttribute("aria-checked"), "true");
+
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight);");
+        By quangNoodleCheckbox = By.xpath("//div[@aria-label='Mì Quảng']");
+
+        // check
+        if (driver.findElement(quangNoodleCheckbox).getAttribute("aria-checked").equals("false")) {
+            driver.findElement(quangNoodleCheckbox).click();
+        }
+        Assert.assertEquals(driver.findElement(quangNoodleCheckbox).getAttribute("aria-checked"), "true");
+
+        // uncheck
+        if (driver.findElement(quangNoodleCheckbox).getAttribute("aria-checked").equals("true")) {
+            driver.findElement(quangNoodleCheckbox).click();
+        }
+        Assert.assertEquals(driver.findElement(quangNoodleCheckbox).getAttribute("aria-checked"), "false");
+
+    }
+
+
     @AfterClass
     public void afterClass() {
 
-            driver.quit();
+        driver.quit();
     }
 }
-//
