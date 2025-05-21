44d62cdb15056a8533b6f691b3df737f5c0591dc
dinkuin <dinhthidiemquynh99@gmail.com>
2025-05-21 22:54:56 +0800

    14/4 Browser_Excercise_user_actionP2

--- selenium/webdriver/Topic_15_User_Action1.java
+++ selenium/webdriver/Topic_15_User_Action1.java
@@ -3,6 +3,7 @@ package webdriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -21,13 +22,13 @@ public class Topic_15_User_Action1 {
 
     @BeforeClass
     public void beforeClass() {
-        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
         action = new Actions(driver);
         WebDriverWait explicitWait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        }
+    }
 
     @Test
     public void TC_01_Hover() throws InterruptedException {
@@ -38,7 +39,7 @@ public class Topic_15_User_Action1 {
         action.moveToElement(ageTextbox).perform();
         Thread.sleep(2000);
 
-        Assert.assertEquals(driver.findElement(By.cssSelector("div.ui-tooltip-content")).getText(),"We ask for your age only for statistical purposes.");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.ui-tooltip-content")).getText(), "We ask for your age only for statistical purposes.");
     }
 
     @Test
@@ -57,7 +58,7 @@ public class Topic_15_User_Action1 {
         // Actions click();
         action.click(driver.findElement(By.xpath("//a[@class='desktop-categoryName' and text()='Home & Bath']"))).perform();
 
-        Assert.assertEquals(driver.findElement(By.cssSelector("span.breadcrumbs-crumb")).getText(),"Kids Home Bath");
+        Assert.assertEquals(driver.findElement(By.cssSelector("span.breadcrumbs-crumb")).getText(), "Kids Home Bath");
     }
 
     @Test
@@ -77,6 +78,6 @@ public class Topic_15_User_Action1 {
 
     @AfterClass
     public void afterClass() {
-            driver.quit();
+        driver.quit();
     }
 }
---
+++ selenium/webdriver/Topic_15_User_Action2.java
@@ -0,0 +1,108 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Topic_15_User_Action2 {
+
+    WebDriver driver;
+    Actions action;
+    String osName = System.getProperty("os.name");
+    Keys keys;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        action = new Actions(driver);
+        // cách 1 để dùng ctr hay command
+        if (osName.startsWith("Windowns")){
+            keys = Keys.CONTROL;
+        } else {
+            keys = Keys.COMMAND;
+        }
+
+        // cách 2 để dùng ctr hay command
+        keys = osName.startsWith("Windowns") ? Keys.CONTROL : Keys.COMMAND;
+    }
+
+    @Test
+    public void TC_01_Click_And_Hold_Block() throws InterruptedException {
+        driver.get("https://automationfc.github.io/jquery-selectable/");
+
+        // Luôn lấy thằng đầu tiên dù có nhiều locator
+        //driver.findElement(By.cssSelector("ol#selecable>li"));
+
+        List<WebElement> allNumber =  driver.findElements(By.cssSelector("ol#selectable>li"));
+
+        action.clickAndHold(allNumber.get(0)) // Click vào số 1 và giữ chuột
+                .moveToElement(allNumber.get(3))  //Di chuột tới số 4
+                .release()  // Nhả chuột trái ra - kết thúc cho sự kiện clickAndHold()
+                .perform();  // Thực thi câu lệnh trên (nếu ko có ko thực thi)
+        Thread.sleep(2000);
+
+        List<WebElement> allNumberSelected = driver.findElements(By.cssSelector("ol#selectable>li.ui-selected"));
+        Assert.assertEquals(allNumberSelected.size(), 4);
+
+    }
+
+    @Test
+    public void TC_02_Click_And_Hold_Random() {
+        driver.get("https://automationfc.github.io/jquery-selectable/");
+
+        List<WebElement> allNumber = driver.findElements(By.cssSelector("ol#selectable>li"));
+        Assert.assertEquals(allNumber.size(), 20);
+
+        // Nhấn phím ctr trc nhưng ko nhả ra
+        action.keyDown(Keys.COMMAND).perform();
+
+        // 1 4 8 11 14 16 18
+        action.click(allNumber.get(0))
+                .click(allNumber.get(3))
+                .click(allNumber.get(7))
+                .click(allNumber.get(10))
+                .click(allNumber.get(13))
+                .click(allNumber.get(15))
+                .click(allNumber.get(17))
+                .pause(Duration.ofSeconds(3))
+                .perform();
+
+        // Nhả ctr ra
+        action.keyUp(Keys.COMMAND).perform();
+
+        List<WebElement> allNumberSelected = driver.findElements(By.cssSelector("ol#selectable>li.ui-selected"));
+        Assert.assertEquals(allNumberSelected.size(), 7);
+
+    }
+
+    @Test
+    public void TC_03_Double_Click() {
+        driver.get("https://automationfc.io/basic-form/index.html");
+
+        action.doubleClick(driver.findElement(By.xpath("//button[text()='Double click me']"))).perform();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("p#demo")).getText(),"Hello Automation Guy!");
+    }
+
+
+
+        @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
